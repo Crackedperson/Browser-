8b9782dd4b34213d9661ac46ef6cb3fce3db3ef9
Shwetank kumar <crackedplayer1202@gmail.com>
2026-08-05 19:59:18 +0530

    Refactor request.js to use environment variable for proxy

--- request.js
+++ request.js
@@ -1,8 +1,20 @@
-require('request-promise')({
+require('dotenv').config();
+const rp = require('request-promise');
+
+const PROXY = process.env.WEBSHARE_PROXY_URL;
+if (!PROXY) {
+  console.error('ERROR: Set WEBSHARE_PROXY_URL in environment or in .env (do not commit .env)');
+  process.exit(1);
+}
+
+rp({
   url: 'http://ipv4.webshare.io/',
-  proxy: 'http://wtqcbrmz:dshl0ygrdwc0@31.59.20.176:6754'
-}).then(function(data){
-  console.log(data); 
-}, function(err){ 
-  console.error(err); 
+  proxy: PROXY,
+  timeout: 15000
+})
+.then(function (data) {
+  console.log(data);
+})
+.catch(function (err) {
+  console.error('Request failed:', err && err.message ? err.message : err);
 });
